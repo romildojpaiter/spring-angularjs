af4089750d1322728903727d59ad27756f82b1d5
romildo <romildopaiter@gmail.com>
2013-12-31 11:12:09 -0300

    Alteração do Carro

--- src/main/java/app/Carro.java
+++ src/main/java/app/Carro.java
@@ -24,6 +24,37 @@ public class Carro {
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((marca == null) ? 0 : marca.hashCode());
+		result = prime * result + ((modelo == null) ? 0 : modelo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carro other = (Carro) obj;
+		if (marca == null) {
+			if (other.marca != null)
+				return false;
+		} else if (!marca.equals(other.marca))
+			return false;
+		if (modelo == null) {
+			if (other.modelo != null)
+				return false;
+		} else if (!modelo.equals(other.modelo))
+			return false;
+		return true;
+	}
 	
 	
 	
