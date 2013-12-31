d5e1ba89e38d883ad987e4d3bd79465eb8d9e3d7
romildo <romildopaiter@gmail.com>
2013-12-31 10:08:06 -0300

    Adicionando a Classe Carro.

---
+++ src/main/java/app/Carro.java
@@ -0,0 +1,30 @@
+package app;
+
+public class Carro {
+	
+	private String modelo;
+	
+	private String marca;
+	
+	public Carro() {
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+	
+}
